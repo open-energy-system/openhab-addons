f6847d38c3fc1ccef69f76644daf1071bbafd27f
Michael <alfa146ti@arcor.de>
2021-09-19 17:20:55 +0200

    Initial creation of binding

---
+++ bundles/org.openhab.binding.cantspro/src/main/java/org/openhab/binding/cantspro/internal/CANtsproConfiguration.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2010-2021 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.cantspro.internal;
+
+/**
+ * The {@link CANtsproConfiguration} class contains fields mapping thing configuration parameters.
+ *
+ * @author Michael Kriegel - Initial contribution
+ */
+public class CANtsproConfiguration {
+
+    /**
+     * Sample configuration parameters. Replace with your own.
+     */
+    public String caninterface;
+}
